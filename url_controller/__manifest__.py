d7823929e6fe04a5bd09e6606fd177e0c871f393
Vinod <vinod@confianzit.biz>
2020-10-28 06:52:58 +0000

    added callback url for Octave

---
+++ url_controller/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'URL Controller',
+    'version': '11.0.1.0.0',
+    'summary': "test kwargs",
+    'category': 'Website',
+    'author': 'Confianz',
+    'maintainer': 'Confianz',
+    'company': 'Confianz',
+    'website': 'https://www.confianzit.com',
+    "depends": ['base'],
+    'data': [
+        'views/call_back_log.xml',
+        'security/ir.model.access.csv',
+    ],
+    'demo_xml': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
